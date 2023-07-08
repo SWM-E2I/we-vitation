530d6c2936128ccb6508dd9b00618a0e20c9d5b7
KAispread <kai@igiuui-MacBookPro.local>
2023-07-08 22:40:39 +0900

    feat(controller): add All controller for this project

---
+++ src/main/java/com/e2i/wemeet/web/controller/RegistrationController.java
@@ -0,0 +1,10 @@
+package com.e2i.wemeet.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/v1/web")
+@Controller
+public class RegistrationController {
+
+}
