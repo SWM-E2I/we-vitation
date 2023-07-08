32017942c39d94029ed1ff2a2c3bb250e16eefbd
KAispread <kai@igiuui-MacBookPro.local>
2023-07-08 13:40:07 +0900

    feat(entity): add member & team entity

---
+++ src/main/java/com/e2i/wemeet/web/domain/member/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.e2i.wemeet.web.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
---
+++ src/main/java/com/e2i/wemeet/web/domain/team/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.e2i.wemeet.web.domain.team;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+}
