afdf578ff7685f85186c2e2288f71148d22690aa
KAispread <kai@igiuui-MacBookPro.local>
2023-07-08 23:37:32 +0900

    feat(phone): add empty phone controller

--- src/main/java/com/e2i/wemeet/web/controller/PhoneController.java
+++ src/main/java/com/e2i/wemeet/web/controller/PhoneController.java
@@ -1,10 +1,16 @@
 package com.e2i.wemeet.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/v1/web")
 @Controller
 public class PhoneController {
+
+    @GetMapping("/phone")
+    public String phone() {
+        return "phone/phone_input";
+    }
 
 }
