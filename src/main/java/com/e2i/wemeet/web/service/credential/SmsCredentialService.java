d6e4d97ad1fd9c922a03a91d4d83f466ad53f3da
KAispread <kai@igiuui-MacBookPro.local>
2023-07-14 16:24:25 +0900

    feat(credential): add credential interface

---
+++ src/main/java/com/e2i/wemeet/web/service/credential/SmsCredentialService.java
@@ -0,0 +1,6 @@
+package com.e2i.wemeet.web.service.credential;
+
+public interface SmsCredentialService extends CredentialService {
+    void verify(final String target, final String credential);
+    boolean isExistMember(final String phone);
+}
