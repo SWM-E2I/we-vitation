95140fa2f04dba1c32d3313729caac1fc5c00c8d
KAispread <kai@igiuui-MacBookPro.local>
2023-07-14 16:25:57 +0900

    feat(credential): add aws resource configuration

---
+++ src/main/java/com/e2i/wemeet/web/config/GlobalConfig.java
@@ -0,0 +1,10 @@
+package com.e2i.wemeet.web.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationPropertiesScan(basePackages = "com.e2i.wemeet.web")
+@Configuration
+public class GlobalConfig {
+
+}
