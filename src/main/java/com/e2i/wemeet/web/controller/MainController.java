dc4c322b029c0e780d8fad340ccc07970003c2c7
KAispread <kai@igiuui-MacBookPro.local>
2023-07-15 16:31:12 +0900

    feat(controller): add controller that phone, main, register

--- src/main/java/com/e2i/wemeet/web/controller/MainController.java
+++ src/main/java/com/e2i/wemeet/web/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.e2i.wemeet.web.controller;
 
-import com.e2i.wemeet.web.service.TeamService;
+import static com.e2i.wemeet.web.controller.CookieEnv.TEAM_CODE;
+
+import com.e2i.wemeet.web.service.team.TeamService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,16 +24,16 @@ public class MainController {
     public String mainPage(@RequestParam String code, HttpServletResponse response, Model model) {
         response.addCookie(generateTeamCodeCookie(code));
 
-        String leaderName = teamService.getTeamLeaderName(code);
-        model.addAttribute("leaderName", leaderName);
+        // String leaderName = teamService.getTeamLeaderName(code);
+        model.addAttribute("leaderName", "leaderName");
 
         return "/main/main_page";
     }
 
     private Cookie generateTeamCodeCookie(String code) {
-        Cookie teamCode = new Cookie("code", code);
+        Cookie teamCode = new Cookie(TEAM_CODE.getName(), code);
         teamCode.setHttpOnly(true);
-        teamCode.setMaxAge(60 * 60 * 3);
+        teamCode.setMaxAge((int) Duration.ofDays(7).getSeconds());
         teamCode.setPath("/");
         teamCode.setSecure(true);
 
