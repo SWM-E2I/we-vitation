d611d210a5b8c535019f6e4c82d410079953567c
KAispread <kai@igiuui-MacBookPro.local>
2023-07-15 16:32:07 +0900

    refactor(exception): change annotation to controller Advice for return html documents

--- src/main/java/com/e2i/wemeet/web/exception/GlobalExceptionController.java
+++ src/main/java/com/e2i/wemeet/web/exception/GlobalExceptionController.java
@@ -4,27 +4,24 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RequiredArgsConstructor
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionController {
 
     private static final String ERROR_LOG_FORMAT = "Error Class : {}, Error Code : {}, Message : {}";
 
-    private static final String UNAUTHORIZED_LOG_FORMAT = "UnAuthorized Error Class : {}, Error Code : {}, Message : {}";
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleAuthorizationException(
+    public String handleAuthorizationException(
         final Exception e) {
-
-        log.info(UNAUTHORIZED_LOG_FORMAT, e.getClass().getSimpleName(), 500,
+        log.info(ERROR_LOG_FORMAT, e.getClass().getSimpleName(), 500,
             e.getMessage());
-        return new ResponseEntity<>(
-            new ErrorResponse(500, e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR
-        );
+
+        return "redirect:/v1/web/error";
     }
 
     public record ErrorResponse(int code, String message) {
