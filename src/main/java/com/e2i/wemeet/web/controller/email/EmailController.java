2d2cd63833bec2e011991fe65fa685fdfa1a8d13
KAispread <kai@igiuui-MacBookPro.local>
2023-07-17 14:47:46 +0900

    fix(argument): add argument resolver to set InvitationInfo from cookie

--- src/main/java/com/e2i/wemeet/web/controller/email/EmailController.java
+++ src/main/java/com/e2i/wemeet/web/controller/email/EmailController.java
@@ -1,7 +1,10 @@
 package com.e2i.wemeet.web.controller.email;
 
+import com.e2i.wemeet.web.global.resolver.Invitation;
+import com.e2i.wemeet.web.global.resolver.InvitationInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/v1/web/email")
@@ -9,7 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmailController {
 
     @GetMapping
-    public String email() {
+    public String email(@Invitation InvitationInfo invitationInfo) {
+        System.out.println("invitationInfo teamCode = " + invitationInfo.teamCode());
+        System.out.println("invitationInfo memberId = " + invitationInfo.memberId());
+
         return "email/email_input";
+    }
+
+    @PostMapping
+    public String issue() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/validate")
+    public String emailValidate() {
+        return "email/email_validate";
+    }
+
+    @PostMapping("/validate")
+    public void credentialValidate(@Invitation InvitationInfo invitationInfo) {
+        System.out.println(invitationInfo);
     }
 }
