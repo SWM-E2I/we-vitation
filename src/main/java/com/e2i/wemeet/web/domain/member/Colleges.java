5ed99e40a4f601b4f49988499fb96c2162544dac
KAispread <kai@igiuui-MacBookPro.local>
2023-07-17 16:26:01 +0900

    feat(domain): add email domain in colleges enum

--- src/main/java/com/e2i/wemeet/web/domain/member/Colleges.java
+++ src/main/java/com/e2i/wemeet/web/domain/member/Colleges.java
@@ -4,19 +4,21 @@ import lombok.Getter;
 
 @Getter
 public enum Colleges {
-    KOREA("고려대학교"),
-    YONSEI("연세대학교"),
-    SNU("서울대학교"),
-    HANYANG("한양대학교"),
-    SOGANG("서강대학교"),
-    EWHA("이화여자대학교"),
-    SEOUL_WOMAN("서울여자대학교"),
-    ANYANG("안양대학교"),
+    KOREA("고려대학교", "korea.ac.kr"),
+    YONSEI("연세대학교", "yonsei.ac.kr"),
+    SNU("서울대학교", "snu.ac.kr"),
+    HANYANG("한양대학교", "hanyang.ac.kr"),
+    SOGANG("서강대학교", "sogang.ac.kr"),
+    EWHA("이화여자대학교", "ewha.ac.kr"),
+    SEOUL_WOMAN("서울여자대학교", "swu.ac.kr"),
+    ANYANG("안양대학교", "anyang.ac.kr"),
     ;
 
     private final String collegeName;
+    private final String emailDomain;
 
-    Colleges(String collegeName) {
+    Colleges(String collegeName, String emailDomain) {
         this.collegeName = collegeName;
+        this.emailDomain = emailDomain;
     }
 }
