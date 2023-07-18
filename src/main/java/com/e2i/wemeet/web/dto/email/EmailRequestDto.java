230d17a2959827074497d9d18e677dfe036520fc
KAispread <kai@igiuui-MacBookPro.local>
2023-07-19 00:25:23 +0900

    feat(email): add validation to emailName

--- src/main/java/com/e2i/wemeet/web/dto/email/EmailRequestDto.java
+++ src/main/java/com/e2i/wemeet/web/dto/email/EmailRequestDto.java
@@ -1,8 +1,10 @@
 package com.e2i.wemeet.web.dto.email;
 
 import com.e2i.wemeet.web.util.validator.bean.EmailDomain;
+import jakarta.validation.constraints.NotEmpty;
 
 public record EmailRequestDto(
+    @NotEmpty
     String emailName,
 
     @EmailDomain
