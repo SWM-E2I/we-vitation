1b9842c3496da9c27bb7ec778c947cb99660aea0
KAispread <kai@igiuui-MacBookPro.local>
2023-07-23 01:01:01 +0900

    refactor(email): change email validation regex

--- src/main/java/com/e2i/wemeet/web/util/validator/module/EmailDomainValidator.java
+++ src/main/java/com/e2i/wemeet/web/util/validator/module/EmailDomainValidator.java
@@ -8,6 +8,7 @@ public class EmailDomainValidator implements ConstraintValidator<EmailDomain, St
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return value.matches("^[^.]+\\.[^.]+$");
+        final String regex = "\\w+\\.ac\\.kr$";
+        return value.matches(regex);
     }
 }
