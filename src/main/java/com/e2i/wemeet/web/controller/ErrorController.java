fadb217326e65e2c340400c94ca824c6efc1fd34
KAispread <kai@igiuui-MacBookPro.local>
2023-07-23 00:58:35 +0900

    feat(error): add page when already has team & team member full

--- src/main/java/com/e2i/wemeet/web/controller/ErrorController.java
+++ src/main/java/com/e2i/wemeet/web/controller/ErrorController.java
@@ -1,8 +1,10 @@
 package com.e2i.wemeet.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/v1/web/error")
 @Controller
@@ -13,4 +15,14 @@ public class ErrorController {
         return "error/error_return";
     }
 
+    @GetMapping("/full")
+    public String errorPageFull() {
+        return "error/error_team_full";
+    }
+
+    @GetMapping("/exist")
+    public String errorPageExist(@RequestParam String leaderName, Model model) {
+        model.addAttribute("leaderName", leaderName);
+        return "error/error_team_exist";
+    }
 }
