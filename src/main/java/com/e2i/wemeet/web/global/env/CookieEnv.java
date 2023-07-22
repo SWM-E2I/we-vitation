631972ab8763349133c6951ec4273519b0af0b15
KAispread <kai@igiuui-MacBookPro.local>
2023-07-22 22:37:34 +0900

    feat(cookie): add cookie delimiter

--- src/main/java/com/e2i/wemeet/web/global/env/CookieEnv.java
+++ src/main/java/com/e2i/wemeet/web/global/env/CookieEnv.java
@@ -5,17 +5,19 @@ import lombok.Getter;
 
 @Getter
 public enum CookieEnv {
-    TEAM_CODE("tc", Duration.ofDays(7)),
-    PERSONAL_IDENTIFIER("idt", Duration.ofDays(7)),
-    PHONE_NUMBER("pn", Duration.ofDays(3)),
-    EMAIL("em", Duration.ofDays(3))
+    TEAM_CODE("tc",  null, Duration.ofDays(7)),
+    PERSONAL_IDENTIFIER("idt", "-", Duration.ofDays(7)),
+    PHONE_NUMBER("pn", null, Duration.ofDays(3)),
+    EMAIL("em", null, Duration.ofDays(3))
     ;
 
     private final String key;
+    private final String delimiter;
     private final Duration expireDuration;
 
-    CookieEnv(String key, Duration expireDuration) {
+    CookieEnv(String key, String delimiter, Duration expireDuration) {
         this.key = key;
+        this.delimiter = delimiter;
         this.expireDuration = expireDuration;
     }
 
