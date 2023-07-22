1a5e746c170d61b4d5a04670763516454ff189a6
KAispread <kai@igiuui-MacBookPro.local>
2023-07-22 22:52:29 +0900

    feat(phone): add route service to redirect page after phone authentication succeed

---
+++ src/main/java/com/e2i/wemeet/web/service/team/RegistrationStep.java
@@ -0,0 +1,14 @@
+package com.e2i.wemeet.web.service.team;
+
+public enum RegistrationStep {
+    BASIC_INFO("기본 정보 입력 단계"),
+    EMAIL_AUTHENTICATION("이메일 인증 단계"),
+    PROFILE_IMAGE("프로필 이미지 등록 단계"),
+    DONE("모든 정보 입력 완료");
+
+    private final String description;
+
+    RegistrationStep(final String description) {
+        this.description = description;
+    }
+}
