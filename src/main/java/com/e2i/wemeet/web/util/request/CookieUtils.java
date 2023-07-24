d380b7662ce2e90f2b76edc3a0bf1aafe72ce573
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-07-24 15:10:19 +0900

    feat(cookie): add cookie logic (#7)
    * refactor(team): remove unnecessary verify method
    
    * style(invitation): change package invitation
    
    * refactor(param): add enum contains parameter name
    
    * feat(member): add email authenticated check method
    
    * feat(cookie): add email cookie key value
    
    * feat(cookie, param): add argument resolver return phoneNumber & email
    
    * refactor(phone, email): change location phone, email convert method
    
    * refactor(resolver): remove unnecessary annotation
    
    * feat(teamCode): add teamCode argument resolver
    
    * style(env): change env package
    
    * refactor(controller): change get value in controller with argument resolver & static method
    
    * style(controller): set controller parameter same level
    
    * feat(cookie): add cookie delimiter
    
    * refactor(cookie): refactor get cookie logic
    
    * refactor(exception): change handling exception when serialize exception occurred
    
    * refactor(s3): remove unused s3 delete method
    
    * fix(phone): add encrypting before adding attribute
    
    * feat(phone): add route service to redirect page after phone authentication succeed
    
    * refactor(team): remove step check member exist
    
    * feat(phone): add phone route controller
    
    * fix(resolver): return dto and add argument resolver
    
    * fix(test): fixup
    
    * feat(error): add page when already has team & team member full
    
    * feat(error): add redirect logic when team member full
    
    * feat(error): add redirect logic when member already has a team
    
    * refactor(email): change email validation regex
    
    * fix(button): fix bug in error page
    
    * style(error): change error message on email format
    
    * feat(team): add validation gender matches MEMBER -> TEAM
    
    * style(team): change team register method
    
    * chore(test): change test data
    
    * refactor(member): change duration member details
    
    * fix(teamCode): fix find teamCode from identifier cookie
    
    * chore(email): Ignore temporary email validation due to sandbox
    
    * chore(init): change test data
    
    * feat(cookie): add cookie
    
    ---------
    
    Co-authored-by: KAispread <kai@igiuui-MacBookPro.local>

--- src/main/java/com/e2i/wemeet/web/util/request/CookieUtils.java
+++ src/main/java/com/e2i/wemeet/web/util/request/CookieUtils.java
@@ -5,6 +5,8 @@ import com.e2i.wemeet.web.global.env.CookieEnv;
 import jakarta.servlet.http.Cookie;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseCookie.ResponseCookieBuilder;
 
 @Slf4j
 public abstract class CookieUtils {
@@ -41,6 +43,15 @@ public abstract class CookieUtils {
         return cookie;
     }
 
+    public static ResponseCookie createResponseCookie(final String value, final CookieEnv cookieEnv) {
+        return ResponseCookie.from(cookieEnv.getKey(), value)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(cookieEnv.getExpireSeconds())
+            .build();
+    }
+  
     public static String getIdentifier(final Cookie[] cookies, final Long memberId) {
         String teamCode = getCookieValue(cookies, CookieEnv.TEAM_CODE);
         return getIdentifier(teamCode, memberId);
